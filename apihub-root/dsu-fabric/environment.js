cc46ddbfdb7d02d9c120f21f61b10a0208312d96
skutner <sava.dumitru.daniel@gmail.com>
2024-01-17 14:52:01 +0200

    Add new environment.js file in dsu-fabric
    The newly created file, environment.js, is added in the dsu-fabric folder. It contains default settings for the DSU_Fabric app, including app name, vault, agent, system, browser details, mode, domain details, enclave type, disabled features, lock features, protocol version, and build version.

---
+++ apihub-root/dsu-fabric/environment.js
@@ -0,0 +1,17 @@
+export default {
+  "appName": "DSU_Fabric",
+  "vault": "server",
+  "agent": "browser",
+  "system": "any",
+  "browser": "any",
+  "mode": "dev-secure",
+  "vaultDomain": "vault",
+  "didDomain": "vault",
+  "epiDomain": "local.epi",
+  "epiSubdomain": "local.epi",
+  "enclaveType": "VersionlessDSUEnclave",
+  "disabledFeatures": "02, 04, 05, 06, 07, 08, 09",
+  "lockFeatures": false,
+  "epiProtocolVersion": 1,
+  "appBuildVersion": "v2023.0.2"
+}
