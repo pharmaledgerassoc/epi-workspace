328fd36dabaec1e14854723bc931395d484008c4
alupoaiemircea <mircea@axiologic.net>
2024-01-19 10:51:28 +0200

    init app update

---
+++ apihub-root/dsu-fabric/web-components/pages/home-page/home-page.js
@@ -0,0 +1,13 @@
+export class HomePage {
+    constructor(element,invalidate) {
+        this.invalidate = invalidate;
+        this.invalidate();
+    }
+    beforeRender() {
+
+    }
+
+    async navigateToProductsPage(){
+
+    }
+}
