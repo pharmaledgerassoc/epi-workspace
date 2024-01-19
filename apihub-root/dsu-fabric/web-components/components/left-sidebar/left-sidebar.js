8b4f8b8c743f6d1481e2464ed7755137ed499e16
Koroem <conduratvictor@yahoo.com>
2024-01-19 13:53:03 +0200

    UI Menu updates and changing menu selection logic

--- apihub-root/dsu-fabric/web-components/components/left-sidebar/left-sidebar.js
+++ apihub-root/dsu-fabric/web-components/components/left-sidebar/left-sidebar.js
@@ -1,13 +1,46 @@
 export class Menu {
     constructor(element,invalidate) {
+        this.element=element;
         this.invalidate = invalidate;
         this.invalidate();
     }
     beforeRender() {
 
     }
+    activateSidebarSelection(_target){
+        const oldLink = this.element.querySelector('#active-menu-item');
+        if (oldLink) {
+            oldLink.removeAttribute('id');
+        }
+        const link= _target.querySelector('a');
+        link.id="active-menu-item";
+    }
+    async navigateToMyAccount(_target) {
+        this.activateSidebarSelection(_target);
+    }
 
-    async navigateToProductsPage(){
+    async navigateToProducts(_target) {
+        this.activateSidebarSelection(_target);
+
+    }
+
+    async navigateToBatches(_target) {
+        this.activateSidebarSelection(_target);
+
+    }
+
+    async navigateToAudit(_target) {
+        this.activateSidebarSelection(_target);
+
+    }
+
+    async navigateToBlockchainStatus(_target) {
+        this.activateSidebarSelection(_target);
+
+    }
+
+    async navigateToLogout(_target) {
+        this.activateSidebarSelection(_target);
 
     }
 }
