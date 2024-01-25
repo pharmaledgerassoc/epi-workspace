84ca083bdf5315a55c219382501818aad3736134
alupoaiemircea <mircea@axiologic.net>
2024-01-25 14:12:14 +0200

    added log entry modal download

--- apihub-root/dsu-fabric/web-components/modals/audit-entry-modal/audit-entry-modal.js
+++ apihub-root/dsu-fabric/web-components/modals/audit-entry-modal/audit-entry-modal.js
@@ -8,17 +8,18 @@ export class AuditEntryModal{
             this.entry = await $$.promisify(webSkel.client.filterAuditLogs)(undefined, undefined, [`pk == ${this.pk}`]);
         });
     }
-    beforeRender(){
-        let cleanObject = function JSONstringifyOrder(obj) {
-            const objToDisplay = {};
-            let displayKeys = ["username", "reason", "status", "itemCode", "diffs", "anchorId", "hashLink", "metadata", "logInfo"];
-            displayKeys.forEach(key => {
-                objToDisplay[key] = obj[key];
-            })
+    JSONstringifyOrder(obj) {
+        const objToDisplay = {};
+        let displayKeys = ["username", "reason", "status", "itemCode", "diffs", "anchorId", "hashLink", "metadata", "logInfo"];
+        displayKeys.forEach(key => {
+            objToDisplay[key] = obj[key];
+        })
 
-            return objToDisplay
-        }
-        this.data = JSON.stringify(cleanObject(this.entry[0]),null, 4);
+        return objToDisplay;
+    }
+    beforeRender(){
+
+        this.data = JSON.stringify(this.JSONstringifyOrder(this.entry[0]),null, 4);
     }
     closeModal(_target) {
         closeModal(_target);
@@ -35,5 +36,16 @@ export class AuditEntryModal{
             modal.style.width = "70%";
             modal.style.maxWidth = "70vw";
         }
+    }
+
+    downloadJSON(){
+        let string = JSON.stringify(this.JSONstringifyOrder(this.entry[0]),null, 4);
+        const blob = new Blob([string], { type: 'application/json' });
+        const downloadLink = document.createElement('a');
+        downloadLink.href = URL.createObjectURL(blob);
+        downloadLink.download = `${this.entry[0].reason}.json`;
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        downloadLink.remove();
     }
 }
