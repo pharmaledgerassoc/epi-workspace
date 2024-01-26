bd403d1ee793d73b64270459df1eda867508bc57
alupoaiemircea <mircea@axiologic.net>
2024-01-26 12:15:12 +0200

    upload files in app epi modal + global scrollbar style

--- apihub-root/dsu-fabric/web-components/modals/add-epi-modal/add-epi-modal.js
+++ apihub-root/dsu-fabric/web-components/modals/add-epi-modal/add-epi-modal.js
@@ -28,4 +28,41 @@ export class AddEpiModal{
             this.element.style.marginLeft = "240px";
         }
     }
+    displaySelectedFiles(input, event) {
+        const files = input.files;
+        const container = this.element.querySelector('.files-table');
+        if ('webkitdirectory' in input) {
+            let stringHMTL = "";
+            for (let i = 0; i < files.length; i++) {
+                stringHMTL += `<div class="file-item">
+                                    <button class="default-button small-padding" data-local-action="removeFile" data-name="${files[i].name}">&times;</button>
+                                    <div class="file-name">${files[i].name}</div>
+                               </div>`;
+            }
+            container.insertAdjacentHTML("beforeend", stringHMTL);
+        } else {
+            container.textContent = 'This browser does not support selecting directories.';
+        }
+    }
+    uploadFiles(){
+        let inputFile = this.element.querySelector("#leaflet");
+        inputFile.removeEventListener("input", this.boundFn);
+        this.boundFn = this.displaySelectedFiles.bind(this, inputFile);
+        inputFile.addEventListener("input", this.boundFn);
+        inputFile.click();
+    }
+    removeFile(_target){
+        let inputFile = this.element.querySelector("#leaflet");
+        let fileName = _target.getAttribute("data-name");
+        let filteredFiles= Array.from(inputFile.files).filter(file => file.name !== fileName);
+        let dataTransfer = new DataTransfer();
+
+        filteredFiles.forEach(file => {
+            dataTransfer.items.add(file);
+        });
+
+        inputFile.files = dataTransfer.files;
+        let item = webSkel.UtilsService.getClosestParentElement(_target, ".file-item");
+        item.remove();
+    }
 }
