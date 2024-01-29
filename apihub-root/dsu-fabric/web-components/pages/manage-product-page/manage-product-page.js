df798606581709cc3b2b9b0359b294d107c22b65
alupoaiemircea <mircea@axiologic.net>
2024-01-29 17:26:43 +0200

    manage product update

--- apihub-root/dsu-fabric/web-components/pages/manage-product-page/manage-product-page.js
+++ apihub-root/dsu-fabric/web-components/pages/manage-product-page/manage-product-page.js
@@ -3,13 +3,43 @@ export class ManageProductPage{
         this.element=element;
         this.invalidate=invalidate;
         this.invalidate();
+        this.leafletTab = `<div class="leaflet-tab inner-tab">
+                    <div class="inner-container">
+                        <div class="no-data">No leaflets added yet</div>
+                        <button class="add-epi-button pointer">+ Add ePI</button>
+                    </div>
+                </div>`;
     }
 
     beforeRender(){
-
+        if(!this.selected){
+            this.tab = this.leafletTab;
+        }
     }
     afterRender(){
-
+        let leaflet = this.element.querySelector("#leaflet");
+        let market = this.element.querySelector("#market");
+        if(this.selected === "market"){
+            market.classList.remove("inactive");
+            market.classList.add("highlighted");
+        }else {
+            leaflet.classList.remove("inactive");
+            leaflet.classList.add("highlighted");
+        }
+    }
+    switchTab(_target){
+        this.selected = _target.getAttribute("id");
+        let tabName = _target.getAttribute("id");
+        let container = this.element.querySelector(".leaflet-market-management");
+        if(tabName === "leaflet"){
+            container.querySelector(".inner-tab").remove();
+            this.tab = this.leafletTab;
+            this.selected = "leaflet";
+            container.insertAdjacentHTML("beforeend", this.tab);
+        }else {
+            this.tab = ``;
+        }
+        this.afterRender();
     }
     uploadPhoto(){
 
