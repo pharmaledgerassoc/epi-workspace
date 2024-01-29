1d79694bd9699b3c64758ecc1a9abc1a5c6fcc0f
Teodor Lupu <luputeodor2@gmail.com>
2024-01-29 10:59:03 +0200

    initial commit manage-product-page

--- apihub-root/dsu-fabric/web-components/pages/products-page/products-page.js
+++ apihub-root/dsu-fabric/web-components/pages/products-page/products-page.js
@@ -75,6 +75,10 @@ export class ProductsPage {
         }
     }
 
+    async navigateToAddProductPage(){
+        await webSkel.changeToDynamicPage("manage-product-page", "manage-products-page");
+    }
+
     toggleSearchIcons(xMark, event){
         if(this.searchInput.value === ""){
             xMark.style.display = "none";
