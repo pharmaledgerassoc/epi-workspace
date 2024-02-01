07997df9b7fabbde8ab1de6dd526a56a59f0d82a
adrian.ganga <adiganga2002@gmail.com>
2024-02-01 16:00:30 +0200

    My account page

--- apihub-root/dsu-fabric/web-components/pages/my-account-page/my-account-page.js
+++ apihub-root/dsu-fabric/web-components/pages/my-account-page/my-account-page.js
@@ -1,4 +1,5 @@
 import constants from "../../../constants.js";
+import {copyToClipboard} from "../../../utils/utils.js";
 
 const openDSU = require("opendsu");
 const config = openDSU.loadAPI("config");
@@ -8,69 +9,38 @@ const scAPI = openDSU.loadAPI("sc");
 export class MyAccountPage {
   constructor(element, invalidate) {
     this.invalidate = invalidate;
-    this.invalidate(this.getCredential);
+    this.invalidate(async () => {
+      await this.fetchAccountData();
+    });
   }
 
   beforeRender() {
-    this.did = "did:ssi:name:vault:DSU_Fabric/devuser";
-    this.jwt = "{\n" +
-      "        \"jwtHeader\": {\n" +
-      "        \"alg\": \"ES256\",\n" +
-      "        \"typ\": \"JWT\"\n" +
-      "        },\n" +
-      "        \"jwtPayload\": {\n" +
-      "        \"sub\": \"did:ssi:group:vault:ePI_Write_Group\",\n" +
-      "        \"iss\": \"did:ssi:name:vault:Demiurge/devuser\",\n" +
-      "        \"nbf\": 1705589611713,\n" +
-      "        \"exp\": 1705621147713,\n" +
-      "        \"iat\": 1705589611713,\n" +
-      "        \"vc\": {\n" +
-      "        \"@context\": [\n" +
-      "        \"https://www.w3.org/2018/credentials/v1\"\n" +
-      "        ],\n" +
-      "        \"type\": [\n" +
-      "        \"VerifiableCredential\"\n" +
-      "        ],\n" +
-      "        \"credentialSubject\": {\n" +
-      "        \"id\": \"did:ssi:group:vault:ePI_Write_Group\"\n" +
-      "        },\n" +
-      "        \"issuer\": \"did:ssi:name:vault:Demiurge/devuser\",\n" +
-      "        \"issuanceDate\": \"2024-01-18T14:53:31Z\",\n" +
-      "        \"expirationDate\": \"2024-01-18T23:39:07Z\"\n" +
-      "        }\n" +
-      "        }\n" +
-      "        }";
-    this.walletSettings = "{\n" +
-      "        \"jwtHeader\": {\n" +
-      "        \"alg\": \"ES256\",\n" +
-      "        \"typ\": \"JWT\"\n" +
-      "        },\n" +
-      "        \"jwtPayload\": {\n" +
-      "        \"sub\": \"did:ssi:group:vault:ePI_Write_Group\",\n" +
-      "        \"iss\": \"did:ssi:name:vault:Demiurge/devuser\",\n" +
-      "        \"nbf\": 1705589611713,\n" +
-      "        \"exp\": 1705621147713,\n" +
-      "        \"iat\": 1705589611713,\n" +
-      "        \"vc\": {\n" +
-      "        \"@context\": [\n" +
-      "        \"https://www.w3.org/2018/credentials/v1\"\n" +
-      "        ],\n" +
-      "        \"type\": [\n" +
-      "        \"VerifiableCredential\"\n" +
-      "        ],\n" +
-      "        \"credentialSubject\": {\n" +
-      "        \"id\": \"did:ssi:group:vault:ePI_Write_Group\"\n" +
-      "        },\n" +
-      "        \"issuer\": \"did:ssi:name:vault:Demiurge/devuser\",\n" +
-      "        \"issuanceDate\": \"2024-01-18T14:53:31Z\",\n" +
-      "        \"expirationDate\": \"2024-01-18T23:39:07Z\"\n" +
-      "        }\n" +
-      "        }\n" +
-      "        }";
   }
 
-  async navigateToProductsPage() {
+  afterRender() {
+    this.renderCredentialContainer(this.credential && this.readableCredential);
+    this.renderSettingsContainer();
+  }
 
+  async copyText() {
+    copyToClipboard(this.did)
+  }
+
+  downloadDebug() {
+    try {
+      let logData = JSON.parse($$.memoryLogger.dump());
+      let formattedJSON = JSON.stringify(logData, null, 4);
+      let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(formattedJSON);
+      let downloadAnchorNode = document.createElement('a');
+      downloadAnchorNode.setAttribute("href", dataStr);
+      downloadAnchorNode.setAttribute("download", "debugLog.json");
+      document.body.appendChild(downloadAnchorNode); // required for firefox
+      downloadAnchorNode.click();
+      downloadAnchorNode.remove();
+    } catch (err) {
+      webSkel.notificationHandler.reportUserRelevantError("Something went wrong on download.", err);
+    }
+    return;
   }
 
   getAppBuildVersion(str) {
@@ -107,69 +77,25 @@ export class MyAccountPage {
     return version;
   }
 
-  async setCredential(credential) {
-    this.credential = credential;
-    this.isInvalidCredential = false;
-    return new Promise((resolve) => {
-      credentialsAPI.parseJWTSegments(this.credential.token, async (parseError, jwtContent) => {
-        if (parseError) {
-          this.isInvalidCredential = true;
-          return resolve(false);
-        }
-        const {jwtHeader, jwtPayload} = jwtContent;
-        this.readableCredential = JSON.stringify({jwtHeader, jwtPayload}, null, 4);
+  renderCredentialContainer(validCredential) {
+    if (!validCredential) {
+      document.querySelector('.invalid-credential').toggle("hidden");
+      return
+    }
+    const readableContainer = document.querySelector('#readableContainer');
+    let readableCredentialElement = readableContainer.querySelector('#readableCredential');
+    if (readableCredentialElement) {
+      readableCredentialElement.remove();
+    }
 
-        const readableContainer = document.querySelector('#readableContainer');
-        let readableCredentialElement = readableContainer.querySelector('#readableCredential');
-        if (readableCredentialElement) {
-          readableCredentialElement.remove();
-        }
-
-        readableCredentialElement = document.createElement('div');
-        readableCredentialElement.id = "readableCredential";
-        readableCredentialElement.language = "json";
-        readableCredentialElement.innerHTML = `<pre><code> ${this.readableCredential} </code></pre>`;
-        readableContainer.appendChild(readableCredentialElement);
-        /*
-         * hidden for MVP1
-        this.DSUStorage.enableDirectAccess(() => {
-          let sc = require("opendsu").loadAPI("sc");
-          sc.getMainDSU((err, mainDSU) => {
-            if (err) {
-               return this.notificationHandler.reportDevRelevantInfo('Error getting mainDSU', err);
-
-               //return console.log('Error getting mainDSU', err);
-            }
-
-
-            mainDSU.getKeySSIAsString((err, keySSI) => {
-                          this.walletKeySSI = keySSI
-                        });
-
-          })
-        });
-     */
-        return resolve(true);
-      });
-    })
-
-
+    readableCredentialElement = document.createElement('div');
+    readableCredentialElement.id = "readableCredential";
+    readableCredentialElement.language = "json";
+    readableCredentialElement.innerHTML = `<pre><code> ${this.readableCredential} </code></pre>`;
+    readableContainer.appendChild(readableCredentialElement);
   }
 
-  async renderSettingsContainer() {
-    let envFile = await $$.promisify(config.readEnvFile)();
-    //hide keySSI properties from display in ui
-    delete envFile["enclaveKeySSI"];
-    delete envFile["sharedEnclaveKeySSI"];
-    //---------- get app version from server env file
-    delete envFile["appBuildVersion"];
-    const environmentJsPath = new URL("environment.js", window.top.location);
-    const response = await fetch(environmentJsPath);
-    const appEnvContent = await response.text();
-    this.model.appVersion = this.getAppBuildVersion(appEnvContent)
-    //-----------------------
-    this.model.editableFeatures = !(!!envFile.lockFeatures);
-    this.model.envData = envFile;
+  renderSettingsContainer() {
     const environmentContainer = document.querySelector('#environmentContainer');
     let environmentDataElement = environmentContainer.querySelector('#environmentData');
     if (environmentDataElement) {
@@ -178,30 +104,40 @@ export class MyAccountPage {
     environmentDataElement = document.createElement('div');
     environmentDataElement.id = "environmentData";
     environmentDataElement.language = "json";
-    environmentDataElement.innerHTML = `<pre><code>${JSON.stringify(envFile, null, 4)}</code></pre>`;
+    environmentDataElement.innerHTML = `<pre><code>${JSON.stringify(this.envData, null, 4)}</code></pre>`;
     environmentContainer.appendChild(environmentDataElement);
   }
 
-  async getCredential() {
+
+  async fetchAccountData() {
     try {
       let mainEnclave = await scAPI.getMainEnclave();
-      try {
-        let did = await scAPI.getMainDIDAsync();
-        this.did = did;
-        let credential = await $$.promisify(mainEnclave.readKey)(constants.CREDENTIAL_KEY);
-        this.displayCredentialArea = !!credential;
-        if (this.displayCredentialArea) {
-          let result = await this.setCredential(credential);
-          /*         if(!result){
-                     return this.showErrorModalAndRedirect('Error parsing user credential', "Error", {tag: "home"})
-                   }*/
-          await this.renderSettingsContainer();
+      let did = await scAPI.getMainDIDAsync();
+      this.did = did;
+      this.credential = await $$.promisify(mainEnclave.readKey)(constants.CREDENTIAL_KEY);
+      if (this.credential) {
+        try {
+          let jwtContent = await $$.promisify(credentialsAPI.parseJWTSegments)(this.credential.token)
+          const {jwtHeader, jwtPayload} = jwtContent;
+          this.readableCredential = JSON.stringify({jwtHeader, jwtPayload}, null, 4);
+        } catch (e) {
+          this.readableCredential = null;
         }
-      } catch (e) {
-        this.displayCredentialArea = false;
       }
-    } catch (err) {
 
+      let envFile = await $$.promisify(config.readEnvFile)();
+      //hide keySSI properties from display in ui
+      delete envFile["enclaveKeySSI"];
+      delete envFile["sharedEnclaveKeySSI"];
+      //---------- get app version from server env file
+      delete envFile["appBuildVersion"];
+      const environmentJsPath = new URL("environment.js", window.top.location);
+      const response = await fetch(environmentJsPath);
+      const appEnvContent = await response.text();
+      this.envData = envFile;
+      this.appVersion = this.getAppBuildVersion(appEnvContent)
+    } catch (err) {
+      webSkel.notificationHandler.reportUserRelevantError("Failed to get wallet data", err);
     }
 
 
