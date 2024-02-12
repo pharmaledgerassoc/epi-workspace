19b53b3e7ff9617a8b453e68f3f8c68e4db41076
skutner <sava.dumitru.daniel@gmail.com>
2024-02-12 11:50:32 +0200

    Refactor code in DSU-fabric and enhance batch operations
    This commit refactors code in DSU-fabric, resulting in improved efficiency and better error handling. The revamped batch operations provide a healthier lifecycle and improved output while revising DSU security codes paves the way for more streamlined system operations. Modifications are primarily done in getSSODetectedId, init function, and integration APIs.

--- apihub-root/dsu-fabric/main.js
+++ apihub-root/dsu-fabric/main.js
@@ -1,41 +1,10 @@
 import {navigateToPage, setupGlobalErrorHandlers} from "./utils/utils.js";
 
 import WebSkel from "./WebSkel/webSkel.js";
-import {
-    gtin,
-    gtin2,
-    batchNumber,
-    batchNumber2,
-    productDetails,
-    productDetails2,
-    batchDetails,
-    batchDetails2,
-    leafletDetails,
-    germanLeaflet,
-    imageData,
-    accessLog,
-    actionLog,
-    actionLog2
-} from "./mockData.js";
 
 const openDSU = require("opendsu");
 const resolver = openDSU.loadAPI("resolver");
-
-const initMockData = async () => {
-    await $$.promisify(webSkel.client.addProduct)(gtin, productDetails);
-    await $$.promisify(webSkel.client.addProduct)(gtin2, productDetails2);
-    await $$.promisify(webSkel.client.addEPI)(gtin, leafletDetails);
-    await $$.promisify(webSkel.client.addProductImage)(gtin, imageData);
-    await $$.promisify(webSkel.client.addBatch)(gtin, batchNumber, batchDetails);
-    await $$.promisify(webSkel.client.addBatch)(gtin2, batchNumber2, batchDetails2);
-    await $$.promisify(webSkel.client.addEPI)(gtin, batchNumber, leafletDetails);
-    await $$.promisify(webSkel.client.updateEPI)(gtin, batchNumber, leafletDetails);
-    await $$.promisify(webSkel.client.addEPI)(gtin, batchNumber, germanLeaflet);
-    await $$.promisify(webSkel.client.addAuditLog)(accessLog);
-    await $$.promisify(webSkel.client.addAuditLog)(actionLog);
-    await $$.promisify(webSkel.client.addAuditLog)(actionLog2);
-}
-
+import env from "./environment.js";
 function saveCurrentState() {
     webSkel.currentState = Object.assign({}, history.state);
 }
@@ -60,9 +29,9 @@ webSkel.notificationHandler = openDSU.loadAPI("error");
     await setupGlobalErrorHandlers();
     window.gtinResolver = require("gtin-resolver");
     let domain = "default";
-    webSkel.client = gtinResolver.getMockEPISORClient(domain);
+    const {epiDomain, epiSubdomain} = env;
+    webSkel.client = gtinResolver.getEPISorClient(epiDomain, epiSubdomain);
 
-    await initMockData();
     webSkel.setDomElementForPages(document.querySelector("#page-content"));
 
     await navigateToPage("landing-page", {["source-page"]: window.location.hash || "#home-page"});
