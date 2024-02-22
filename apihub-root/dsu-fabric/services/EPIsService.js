dab2966107b327c7675b7be92b54b0963c6f4b11
adrian.ganga <adiganga2002@gmail.com>
2024-02-22 13:16:57 +0200

    axiologic-pla/dev#640 update EPI files front/back validation

--- apihub-root/dsu-fabric/services/EPIsService.js
+++ apihub-root/dsu-fabric/services/EPIsService.js
@@ -1,6 +1,7 @@
 import {getTextDirection} from "../utils/utils.js";
 import constants from "../constants.js";
 
+
 export class EPIsService {
     constructor() {
     }
@@ -69,5 +70,68 @@ export class EPIsService {
         let epiUnit = epiUnits.find(unit => unit.id === id);
         epiUnit.action = "delete";
         return epiUnit;
+    }
+
+    async validateEPIFilesContent(epiFiles) {
+        try {
+            let xmlContent;
+            let epiImages = {};
+            for (let file of epiFiles) {
+                if (file.name.endsWith(".xml")) {
+                    xmlContent = await gtinResolver.DSUFabricUtils.getFileContent(file);
+                } else {
+                    let fileContent = await gtinResolver.DSUFabricUtils.getFileContentAsBuffer(file);
+                    epiImages[file.name] = gtinResolver.utils.getImageAsBase64(fileContent);
+                }
+            }
+
+            let xmlService = new gtinResolver.XMLDisplayService(document.querySelector(".modal-body"));
+
+            let htmlXMLContent = xmlService.getHTMLFromXML("", xmlContent);
+            let leafletHtmlContent = xmlService.buildLeafletHTMLSections(htmlXMLContent);
+            if (!leafletHtmlContent) {
+                webSkel.notificationHandler.reportUserRelevantError(this.getToastContent(this.generateMissingToastList(missingImgFiles)));
+                return false;
+
+            }
+
+            let leafletHtmlImages = htmlXMLContent.querySelectorAll("img");
+            let htmlImageNames = Array.from(leafletHtmlImages).map(img => img.getAttribute("src"));
+            //removing from validation image src that are data URLs ("data:....")
+            htmlImageNames = htmlImageNames.filter((imageSrc) => {
+                let dataUrlRegex = new RegExp(/^\s*data:([a-z]+\/[a-z]+(;[a-z\-]+\=[a-z\-]+)?)?(;base64)?,[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*$/i);
+                if (!!imageSrc.match(dataUrlRegex) || imageSrc.startsWith("data:")) {
+                    return false;
+                }
+                return true;
+            });
+            let uploadedImageNames = Object.keys(epiImages);
+            let differentCaseImgFiles = [];
+            let missingImgFiles = []
+            htmlImageNames.forEach(imgName => {
+                if (!epiImages[imgName]) {
+                    let differentCaseImg = uploadedImageNames.find((item) => item.toLowerCase() === imgName.toLowerCase())
+                    if (differentCaseImg) {
+                        differentCaseImgFiles.push({xmlName: imgName, fileName: differentCaseImg});
+                    } else {
+                        missingImgFiles.push(imgName);
+                    }
+                }
+            })
+
+            if (missingImgFiles.length > 0) {
+                webSkel.notificationHandler.reportUserRelevantError(this.getToastContent(this.generateMissingToastList(missingImgFiles)));
+                return false;
+            }
+            if (differentCaseImgFiles.length > 0) {
+                webSkel.notificationHandler.reportUserRelevantWarning(this.getToastContent(this.generateDifferentCaseToastList(differentCaseImgFiles)));
+            }
+            return true;
+        } catch (e) {
+            console.log("EPI files validation fails: ", e);
+            webSkel.notificationHandler.reportUserRelevantError("Attention: uploaded files format is not supported. To proceed successfully verify that you have an XML file and your XML file adheres to the prescribed format and structure. To obtain the correct XML specifications we recommend consulting our documentation. Thank you! ");
+            return false;
+        }
+
     }
 }
