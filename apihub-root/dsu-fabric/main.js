12b5238d7648c1d53344d53d396d1da13d80fa22
adrian.ganga <adiganga2002@gmail.com>
2024-02-28 14:17:46 +0200

    axiologic-pla/dev#640 add loader on first app load

--- apihub-root/dsu-fabric/main.js
+++ apihub-root/dsu-fabric/main.js
@@ -3,27 +3,17 @@ import {navigateToPage, setupGlobalErrorHandlers} from "./utils/utils.js";
 import WebSkel from "./WebSkel/webSkel.js";
 
 const openDSU = require("opendsu");
-const resolver = openDSU.loadAPI("resolver");
+
 import env from "./environment.js";
+
 function saveCurrentState() {
     webSkel.currentState = Object.assign({}, history.state);
-}
-
-function closeDefaultLoader() {
-    let UILoader = {
-        "modal": document.querySelector('#default-loader-markup'),
-        "style": document.querySelector('#default-loader-style'),
-        "script": document.querySelector('#default-loader-script')
-    }
-    UILoader.modal.close();
-    UILoader.modal.remove();
-    UILoader.script.remove();
-    UILoader.style.remove();
 }
 
 window.webSkel = await WebSkel.initialise("./webskel-configs.json");
 
 webSkel.notificationHandler = openDSU.loadAPI("error");
+
 (async () => {
     webSkel.setLoading(`<div class="spinner-container"><div class="spin"></div></div>`);
     await setupGlobalErrorHandlers();
@@ -33,6 +23,10 @@ webSkel.notificationHandler = openDSU.loadAPI("error");
     webSkel.client = gtinResolver.getEPISorClient(epiDomain, epiSubdomain);
 
     webSkel.setDomElementForPages(document.querySelector("#page-content"));
+
+    const loader = document.querySelector("#before-webskel-loader");
+    loader.close(); // Close the loader
+    loader.remove();
 
     await navigateToPage("landing-page", {["source-page"]: window.location.hash || "#home-page"});
     window.addEventListener('beforeunload', saveCurrentState);
