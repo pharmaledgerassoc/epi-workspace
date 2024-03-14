44f8a7455851018b616653e3dc811b392d6a76bc
skutner <sava.dumitru.daniel@gmail.com>
2024-03-14 12:09:40 +0200

    Refactor session expiration and logout checks order
    Reordered the checks for session expiration and logout status in both Timer.js files in dsu-fabric and demiurge-wallet directories. Now, it primarily checks for session expiration before checking the logout status. This provides a more logical flow and ensures that expired sessions are handled first. Additionally, the sessionHandler function was moved to a global scope which allows for appropriate interval clearing in distributed situations.

--- apihub-root/dsu-fabric/services/Timer.js
+++ apihub-root/dsu-fabric/services/Timer.js
@@ -7,15 +7,16 @@ window.sessionHandler = setInterval(async () => {
     response = await fetch(LOGOUT_WAS_TRIGGERED_URL);
     const logout = await response.text();
 
-    if(logout === "true"){
-        document.body.style.setProperty("visibility","hidden");
-        alert("A Single Sign-On (SSO) login process is detected to be in progress across multiple browser instances. If this assessment is incorrect, kindly terminate all browser instances and recommence the login procedure.");
-        return;
-    }
     document.body.style.setProperty("visibility","visible");
     if (sessionHasExpired === "true") {
         clearInterval(window.sessionHandler);
         window.disableRefreshSafetyAlert = true;
         window.location = "/logout"
+        return
+    }
+
+    if(logout === "true"){
+        document.body.style.setProperty("visibility","hidden");
+        alert("A Single Sign-On (SSO) login process is detected to be in progress across multiple browser instances. If this assessment is incorrect, kindly terminate all browser instances and recommence the login procedure.");
     }
 }, 10000);
