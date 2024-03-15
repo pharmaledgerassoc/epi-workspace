4de9210cea592dcf8fbd1664a16126e3c6cf1ce4
alupoaiemircea <mircea@axiologic.net>
2024-03-15 11:55:53 +0200

    recovery modal UI

--- apihub-root/dsu-fabric/web-components/modals/dialog-modal/dialog-modal.js
+++ apihub-root/dsu-fabric/web-components/modals/dialog-modal/dialog-modal.js
@@ -1,7 +1,8 @@
-export class DialogModal {
+import {CommonPresenterClass} from "../../CommonPresenterClass.js";
+
+export class DialogModal extends CommonPresenterClass {
     constructor(element, invalidate) {
-        this.element = element;
-        this.invalidate = invalidate;
+        super(element, invalidate);
 
         this.message = this.element.variables["data-message"];
         this.header = this.element.variables["data-header"];
@@ -21,5 +22,23 @@ export class DialogModal {
 
     accept(_target){
        webSkel.closeModal(_target, true);
+    }
+
+    switchModalView(){
+        let modal = webSkel.getClosestParentElement(this.element,"dialog");
+        if(!modal.getAttribute("data-expanded")){
+            modal.setAttribute("data-expanded", "true")
+            modal.style.width = "95%";
+            modal.style.maxWidth = "95vw";
+            this.element.style.marginLeft = "0";
+        }else {
+            modal.removeAttribute("data-expanded");
+            modal.style.width = "75%";
+            modal.style.maxWidth = "75vw";
+            this.element.style.marginLeft = "240px";
+        }
+    }
+    closeModal(_target) {
+        webSkel.closeModal(_target);
     }
 }
