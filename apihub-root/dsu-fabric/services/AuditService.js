3ab6d4edb50b55dfd3d975865983d48d308fa7f2
adrian.ganga <adiganga2002@gmail.com>
2024-03-26 15:58:20 +0200

    axiologic-pla/dev#640 update diff show details in csv and header label

--- apihub-root/dsu-fabric/services/AuditService.js
+++ apihub-root/dsu-fabric/services/AuditService.js
@@ -7,7 +7,7 @@ export class AuditService {
     objectToArray(item, type) {
 
         if (type === "action") {
-            return [`'${item.itemCode}'`, item.batchNumber ? `'${item.batchNumber}'` : "-", item.reason, item.username ? `'${item.username}'` : "", new Date(item.__timestamp).toISOString()];
+            return [`'${item.itemCode}'`, item.batchNumber ? `'${item.batchNumber}'` : "-", item.reason, item.username ? `'${item.username}'` : "", new Date(item.__timestamp).toISOString(), JSON.stringify(item.details)];
         }
 
         if (type === "access") {
@@ -22,15 +22,15 @@ export class AuditService {
     convertToCSV(items, type) {
         let headers;
         if (type === "action") {
-            headers = ["Product Code", "Batch number", "Reason", "User", "Creation Time"];
+            headers = ["Product Code", "Batch number", "Reason", "User", "Creation Time", "Data"];
         }
         if (type === "access") {
             headers = ["User", "Action", "User DID", "User Group", "Creation Time"];
         }
-        let columnTitles = headers.join(",") + "\n";
+        let columnTitles = headers.join(";") + "\n";
         let rows = "";
         for (let item of items) {
-            item = this.objectToArray(item, type).join(",");
+            item = this.objectToArray(item, type).join(";");
             rows += item + "\n";
         }
         return [columnTitles + rows];
