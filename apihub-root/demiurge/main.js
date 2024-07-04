15d0bb5df7d6222ab1850d1438a09d287d7007e0
alupoaiemircea <mircea@axiologic.net>
2024-07-04 10:19:36 +0300

    keep current state from url

--- apihub-root/demiurge/main.js
+++ apihub-root/demiurge/main.js
@@ -4,5 +4,10 @@ import WebSkel from "./WebSkel/webSkel.js";
     let pageContent = document.querySelector("#page-content");
     pageContent.insertAdjacentHTML("beforebegin", `<left-sidebar data-presenter="left-sidebar"></left-sidebar>`)
     webSkel.setDomElementForPages(pageContent);
+    let currentPage = window.location.hash.slice(1);
+    if(currentPage === ""){
+        currentPage = "groups-page";
+    }
+    await webSkel.changeToDynamicPage(currentPage, currentPage);
 })();
 
