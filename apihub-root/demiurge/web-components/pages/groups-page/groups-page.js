23c6300c3cc3ff4528cee1e6604d02a4d632249e
Koroem <conduratvictor@yahoo.com>
2024-07-04 16:26:08 +0300

    Markup and css for groups-page, and populating with mock data

---
+++ apihub-root/demiurge/web-components/pages/groups-page/groups-page.js
@@ -0,0 +1,479 @@
+const mockData = {
+    administrationGroup: [
+        {
+            username: 'DSU_Fabric/AdminWarrior',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminWarrior',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminMage',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminMage',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminRogue',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminRogue',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminPaladin',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminPaladin',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminCleric',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminCleric',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminBard',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminBard',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminDruid',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminDruid',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminSorcerer',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminSorcerer',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminNecromancer',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminNecromancer',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminMonk',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminMonk',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminRanger',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminRanger',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminKnight',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminKnight',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminBarbarian',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminBarbarian',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminAssassin',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminAssassin',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminWizard',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminWizard',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminWarlock',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminWarlock',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminBerserker',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminBerserker',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminTemplar',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminTemplar',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminCrusader',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminCrusader',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        },
+        {
+            username: 'DSU_Fabric/AdminWitch',
+            DID: 'did:ssi:name:vault:DSU_Fabric/AdminWitch',
+            group: 'ePI Administration Group',
+            groupDID: 'did:ssi:group:vault:ePI_Administration_Group'
+        }
+    ],
+    writeGroup: [
+        {
+            username: 'DSU_Fabric/WriteWarrior',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteWarrior',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteMage',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteMage',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteRogue',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteRogue',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WritePaladin',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WritePaladin',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteCleric',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteCleric',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteBard',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteBard',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteDruid',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteDruid',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteSorcerer',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteSorcerer',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteNecromancer',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteNecromancer',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteMonk',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteMonk',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteRanger',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteRanger',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteKnight',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteKnight',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteBarbarian',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteBarbarian',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteAssassin',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteAssassin',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteWizard',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteWizard',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteWarlock',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteWarlock',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteBerserker',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteBerserker',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteTemplar',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteTemplar',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteCrusader',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteCrusader',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        },
+        {
+            username: 'DSU_Fabric/WriteWitch',
+            DID: 'did:ssi:name:vault:DSU_Fabric/WriteWitch',
+            group: 'ePI Write Group',
+            groupDID: 'did:ssi:group:vault:ePI_Write_Group'
+        }
+    ],
+    readGroup: [
+        {
+            username: 'DSU_Fabric/ReadWarrior',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadWarrior',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadMage',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadMage',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadRogue',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadRogue',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadPaladin',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadPaladin',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadCleric',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadCleric',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadBard',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadBard',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadDruid',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadDruid',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadSorcerer',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadSorcerer',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadNecromancer',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadNecromancer',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadMonk',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadMonk',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadRanger',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadRanger',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadKnight',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadKnight',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadBarbarian',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadBarbarian',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadAssassin',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadAssassin',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadWizard',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadWizard',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadWarlock',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadWarlock',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadBerserker',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadBerserker',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadTemplar',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadTemplar',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadCrusader',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadCrusader',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        },
+        {
+            username: 'DSU_Fabric/ReadWitch',
+            DID: 'did:ssi:name:vault:DSU_Fabric/ReadWitch',
+            group: 'ePI Read Group',
+            groupDID: 'did:ssi:group:vault:ePI_Read_Group'
+        }
+    ]
+};
+
+const groupNames = {
+    Administration: "ePI Administration Group",
+    Write: "ePI Write Group",
+    Read: "ePI Read Group"
+}
+
+export class GroupsPage {
+    constructor(element, invalidate) {
+        this.element = element;
+        this.invalidate = invalidate;
+        this.invalidate(async () => {
+            this.groupMembers = await this.getGroupData("Administration");
+        });
+    }
+
+    getGroupData(groupId) {
+        const getAdministrationGroupMembers = () => {
+            return mockData["administrationGroup"]
+        }
+        const getWriteGroupMembers = () => {
+            return mockData["writeGroup"]
+        }
+        const getReadGroupMembers = () => {
+            return mockData["readGroup"]
+        }
+        switch (groupId) {
+            case "Administration":
+                return getAdministrationGroupMembers();
+            case "Write":
+                return getWriteGroupMembers();
+            case "Read":
+                return getReadGroupMembers();
+            default:
+                return getAdministrationGroupMembers();
+        }
+    }
+
+    beforeRender() {
+        const renderAdministrationGroup = () => {
+            this.selectedTab = "Administration";
+        }
+        const renderWriteGroup = () => {
+        }
+        const renderReadGroup = () => {
+        }
+        switch (this.selectedTab) {
+            case "Administration":
+                renderAdministrationGroup()
+                break;
+            case "Write":
+                renderWriteGroup()
+                break;
+            case "Read":
+                renderReadGroup()
+                break;
+            default:
+                renderAdministrationGroup()
+        }
+        this.groupName = `"` + groupNames[this.selectedTab] + `"`;
+        if (this.groupMembers.length > 0) {
+            this.groupMembersMarkup = this.groupMembers
+                .map(member => `<group-member data-presenter="group-member" data-username="${member.username}" data-group="${member.group}" data-userDID="${member.DID} data-groupDID="${member.groupDID}"></group-member>`)
+                .join("");
+        } else {
+            this.groupMembersMarkup = `<div class="empty-group-container">There are no members in this group!</div>`;
+        }
+
+    }
+
+    afterRender() {
+        this.selectTab(this.selectedTab);
+            const input = document.getElementById('member-did-text');
+            const addMemberButton=document.getElementById('add-member-button');
+            input.addEventListener('input', () => {
+                if (input.value.trim() !== "") {
+                    addMemberButton.disabled = false;
+                } else {
+                    addMemberButton.disabled = true;
+                }
+            });
+    }
+
+    changeTab(_target, groupId) {
+        const currentSelectedTab = this.element.querySelector('.selected');
+        if (currentSelectedTab) {
+            currentSelectedTab.classList.remove('selected');
+        }
+        this.selectedTab = groupId;
+        this.invalidate(async () => {
+            this.groupMembers = await this.getGroupData(groupId)
+        });
+    }
+
+    selectTab(groupId) {
+        const selectedTab = this.element.querySelector(`#${groupId}`);
+        selectedTab?.classList.add('selected');
+    }
+    async paste(_target){
+        window.focus();
+        try {
+            const input = document.getElementById('member-did-text');
+            const text = await navigator.clipboard.readText();
+            const pasteLocation=this.element.querySelector('#member-did-text');
+            pasteLocation.value=text;
+            input.dispatchEvent(new Event('input'));
+        } catch (err) {
+            console.error('Failed to read clipboard contents: ', err);
+        }
+    }
+}
