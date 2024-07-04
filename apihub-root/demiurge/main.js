a9eb1e8c4e2850caab58634696c76170489c4d95
alupoaiemircea <mircea@axiologic.net>
2024-07-04 09:34:05 +0300

    update sidebar

--- apihub-root/demiurge/main.js
+++ apihub-root/demiurge/main.js
@@ -2,7 +2,7 @@ import WebSkel from "./WebSkel/webSkel.js";
 (async ()=>{
     window.webSkel = await WebSkel.initialise("./webskel-configs.json");
     let pageContent = document.querySelector("#page-content");
-    pageContent.insertAdjacentHTML("afterbegin", `<left-sidebar data-presenter="left-sidebar"></left-sidebar>`)
+    pageContent.insertAdjacentHTML("beforebegin", `<left-sidebar data-presenter="left-sidebar"></left-sidebar>`)
     webSkel.setDomElementForPages(pageContent);
 })();
 
