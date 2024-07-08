dca80c20eb9784b1e249360b0d33f66223a33c65
Koroem <conduratvictor@yahoo.com>
2024-07-08 13:39:22 +0300

    Create-identity-modal and break-glass-recover-code modal

--- apihub-root/demiurge/web-components/pages/booting-identity-page/booting-identity-page.js
+++ apihub-root/demiurge/web-components/pages/booting-identity-page/booting-identity-page.js
@@ -1,10 +1,21 @@
-export class BootingIdentityPage{
+export class BootingIdentityPage {
     constructor(element, invalidate) {
         this.element = element;
         this.invalidate = invalidate;
         this.invalidate();
     }
-    beforeRender(){
+
+    beforeRender() {
+
+    }
+
+    async createIdentity() {
+        const initialiseIdentityModal = await webSkel.showModal("create-identity-modal");
+        setTimeout(async () => {
+            initialiseIdentityModal.close();
+            initialiseIdentityModal.remove();
+            const createdIdentityModal = await webSkel.showModal("break-glass-recovery-code-modal");
+        }, 4000); //artificial delay for UI testing
 
     }
 }
