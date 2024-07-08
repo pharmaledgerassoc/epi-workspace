3cf6cfb167e60b412089ebb76426c58cdbf314ed
alupoaiemircea <mircea@axiologic.net>
2024-07-08 13:49:34 +0300

    update booting-identity-page spinner

--- apihub-root/demiurge/web-components/pages/booting-identity-page/booting-identity-page.js
+++ apihub-root/demiurge/web-components/pages/booting-identity-page/booting-identity-page.js
@@ -6,10 +6,13 @@ export class BootingIdentityPage {
     }
 
     beforeRender() {
-
+        this.username = "John Doe";
     }
 
-    async createIdentity() {
+    async createIdentity(_target) {
+        _target.classList.add("disabled-btn");
+        _target.innerHTML = `<i class="fa fa-circle-o-notch fa-spin" style="font-size:18px; width: 18px; height: 18px;"></i>`;
+        _target.classList.add("remove");
         const initialiseIdentityModal = await webSkel.showModal("create-identity-modal");
         setTimeout(async () => {
             initialiseIdentityModal.close();
