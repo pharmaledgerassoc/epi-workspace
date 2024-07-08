f1445d1da2f678abe61d96285bcb1e1054890e6d
Koroem <conduratvictor@yahoo.com>
2024-07-08 10:39:21 +0300

    Global Functions to copy and paste data into/from input and textarea fields

--- apihub-root/demiurge/main.js
+++ apihub-root/demiurge/main.js
@@ -1,29 +1,77 @@
 import WebSkel from "./WebSkel/webSkel.js";
+
 const openDSU = require("opendsu");
 import {getInstance} from "./MockClient.js";
 import mockData from "./MockData.js";
-(async ()=>{
+
+function registerGlobalActions() {
+    async function pasteToField(_target,fieldId) {
+        window.focus();
+
+        const pasteInputLocation = document.getElementById(fieldId);
+        if (!pasteInputLocation) {
+            throw new Error(`Element with id ${fieldId} not found.`);
+        }
+
+        if (pasteInputLocation.readOnly) {
+            throw new Error(`Element with id ${fieldId} is readonly.`);
+        }
+
+        const validInputTypes = ['text', 'textarea'];
+        if (!validInputTypes.includes(pasteInputLocation.type)) {
+            throw new Error(`Element with id ${fieldId} is not a valid input type.`);
+        }
+
+        const text = await navigator.clipboard.readText();
+        pasteInputLocation.value = text;
+        pasteInputLocation.dispatchEvent(new Event('input'));
+    }
+
+    function copyFieldValue(_target,fieldId) {
+        const element = document.getElementById(fieldId);
+        if (!element) {
+            throw new Error(`Element with id ${fieldId} not found`);
+        }
+
+
+        const validInputTypes = ['text', 'textarea'];
+        if (!validInputTypes.includes(element.type)) {
+            throw new Error(`Element with id ${fieldId} is not a valid input type`);
+        }
+
+        window.focus();
+        navigator.clipboard.writeText(element.value)
+            .then(() => console.log('Text copied to clipboard'))
+            .catch(err => console.error('Error copying text: ', err));
+    }
+
+    webSkel.registerAction("pasteToField", pasteToField);
+    webSkel.registerAction("copyFieldValue", copyFieldValue);
+}
+
+(async () => {
     window.webSkel = await WebSkel.initialise("./webskel-configs.json");
+    registerGlobalActions();
     webSkel.setLoading(`<div class="spinner-container"><div class="spin"></div></div>`);
     let pageContent = document.querySelector("#page-content");
     pageContent.insertAdjacentHTML("beforebegin", `<sidebar-menu data-presenter="left-sidebar"></sidebar-menu>`)
     webSkel.setDomElementForPages(pageContent);
     let currentPage = window.location.hash.slice(1);
     let presenterName = currentPage.split("/")[0];
-    if(currentPage === ""){
+    if (currentPage === "") {
         currentPage = "groups-page";
         presenterName = "groups-page";
     }
     webSkel.notificationHandler = openDSU.loadAPI("error");
     webSkel.client = getInstance("default");
     let promises = []
-    for(let item of mockData.devUserLogs){
+    for (let item of mockData.devUserLogs) {
         promises.push($$.promisify(webSkel.client.addAuditLog)(item));
     }
-    for(let item of mockData.userLogs){
+    for (let item of mockData.userLogs) {
         promises.push($$.promisify(webSkel.client.addAuditLog)(item));
     }
-    for(let item of mockData.healthChecks){
+    for (let item of mockData.healthChecks) {
         promises.push($$.promisify(webSkel.client.addHealthCheck)(item));
     }
     await Promise.all(promises);
