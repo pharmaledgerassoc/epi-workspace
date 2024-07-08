e806183ad2c6ab85367daa53d221e3f14f784929
Koroem <conduratvictor@yahoo.com>
2024-07-08 12:23:21 +0300

    Data-recovery-key modal

--- apihub-root/demiurge/web-components/modals/data-recovery-key-modal/data-recovery-key-modal.js
+++ apihub-root/demiurge/web-components/modals/data-recovery-key-modal/data-recovery-key-modal.js
@@ -5,9 +5,17 @@ export class DataRecoveryKeyModal{
         this.invalidate();
     }
     beforeRender(){
-
+        this.dataRecoveryKey="XLEXhsLoqwXxEWxk9nFGaX6wZY4CkiNsvfzFdMNPTPAUcxLSn11mjecNksmrAJtMGj5MGmaT67AZ9uCpTfbHD9    "
     }
     afterRender(){
-
+        const input = document.getElementById('data-recovery-key');
+        const addMemberButton=document.getElementById('submitDataRecoveryKey');
+        input.addEventListener('input', () => {
+            if (input.value.trim() !== "") {
+                addMemberButton.disabled = false;
+            } else {
+                addMemberButton.disabled = true;
+            }
+        });
     }
 }
