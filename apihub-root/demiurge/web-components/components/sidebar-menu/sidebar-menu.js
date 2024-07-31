7e98efabd50abbe10d4221360139df9fbc53ba8c
adrian.ganga <adiganga2002@gmail.com>
2024-07-31 14:52:27 +0300

    add logout

--- apihub-root/demiurge/web-components/components/sidebar-menu/sidebar-menu.js
+++ apihub-root/demiurge/web-components/components/sidebar-menu/sidebar-menu.js
@@ -1,27 +1,26 @@
 export class SidebarMenu {
-    constructor(element,invalidate) {
+    constructor(element, invalidate) {
         this.element = element;
         this.invalidate = invalidate;
         this.invalidate();
     }
-    beforeRender(){
-        if(window.location.hash.slice(1).split("/")[0] === "booting-identity-page"){
-            this.element.classList.add("no-identity");
-            this.hasIdentity = false;
-        }
+
+    afterRender() {
+        this.highlightCurrentSelection(window.location.hash.slice(1));
     }
-    afterRender(){
-        if(this.hasIdentity){
-            this.highlightCurrentSelection(window.location.hash.slice(1));
+
+    async changePage(_target, page) {
+        if (page === 'logout') {
+            sessionStorage.setItem("initialURL", window.location.href);
+            window.top.location = "/logout";
         }
-    }
-    async changePage(_target, page){
         await webSkel.changeToDynamicPage(page, `${page}`);
         this.highlightCurrentSelection(page);
     }
-    highlightCurrentSelection(page){
+
+    highlightCurrentSelection(page) {
         let element = this.element.querySelector(`#active`);
-        if(element){
+        if (element) {
             element.removeAttribute("id");
         }
         page = page.split("/")[0];
