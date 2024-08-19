b334e025d34c8cfb42391b6bd005f0b4c2cfe5d0
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2024-08-19 16:45:09 +0300

    my identity page impl

--- apihub-root/demiurge/web-components/pages/my-identities-page/my-identities-page.js
+++ apihub-root/demiurge/web-components/pages/my-identities-page/my-identities-page.js
@@ -1,18 +1,39 @@
+import AppManager from "./../../../services/AppManager.js";
+
 export class MyIdentitiesPage{
     constructor(element, invalidate) {
         this.element = element;
         this.invalidate = invalidate;
         this.invalidate(async ()=>{
-            this.did = "did:ssi:name:vault:Demiurge/devuser";
+            let appManager = AppManager.getInstance();
+            try{
+                this.did = await appManager.getDID();
+            }catch (err){
+                this.did = " - ";
+                this.preventClick = true;
+                return webSkel.notificationHandler.reportUserRelevantError("Failed to read DID", err);
+            }
         });
+        webSkel.registerAction("copyIdentity", this.copyIdentity);
     }
+
     beforeRender(){
     }
+
     afterRender(){
+        if(this.preventClick){
+            this.element.querySelector("#copy").classList.add("disabled");
+            return;
+        }
         let input = this.element.querySelector(".did-identity");
         input.value = this.did;
     }
+
     async copyIdentity(){
+        if(this.classList.contains("disabled")){
+            //if we fail to display did, we disable the copy mechanism!
+            return ;
+        }
         let input = this.element.querySelector(".did-identity");
         input.select();
         input.setSelectionRange(0, 99999); // For mobile devices
