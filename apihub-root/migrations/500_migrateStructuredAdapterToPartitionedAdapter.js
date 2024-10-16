51bb91e22bcf04c440bc4460b06b44a8cbc05fe3
skutner <sava.dumitru.daniel@gmail.com>
2024-10-16 16:32:38 +0300

    Refactor migration script into modular functions
    Refactored the script to include utility functions for folder renaming, file updating, and table migration. Introduced a combined migration process for both DSU Fabric and FixedUrls.db, improving modularity and readability.

--- apihub-root/migrations/500_migrateStructuredAdapterToPartitionedAdapter.js
+++ apihub-root/migrations/500_migrateStructuredAdapterToPartitionedAdapter.js
@@ -1,29 +1,55 @@
 const LokiEnclaveFacade = require("loki-enclave-facade");
-const apihubModule = require("apihub");
-require("opendsu")
-const adapters = LokiEnclaveFacade.Adapters;
 const path = require('path');
 const fs = require('fs');
-const apihubRootFolder = apihubModule.getServerConfig().storage;
-const DSU_FABRIC_ENCLAVE_RENAMED_FOLDER_NAME = 'dsu-fabric-enclave-renamed';
-const DSU_FABRIC_ENCLAVE_FOLDER_NAME = `DB_${process.env.EPI_DOMAIN}_${process.env.EPI_SUBDOMAIN}`;
-const DSU_FABRIC_ENCLAVE_FOLDER_PATH = path.join(apihubRootFolder, "external-volume/lightDB", DSU_FABRIC_ENCLAVE_FOLDER_NAME);
-const DSU_FABRIC_ENCLAVE_PATH = path.join(DSU_FABRIC_ENCLAVE_FOLDER_PATH, "database");
-const DSU_FABRIC_ENCLAVE_RENAMED_FOLDER_PATH = path.join(apihubRootFolder, "external-volume/lightDB", DSU_FABRIC_ENCLAVE_RENAMED_FOLDER_NAME);
-const DSU_FABRIC_ENCLAVE_RENAMED_PATH = path.join(DSU_FABRIC_ENCLAVE_RENAMED_FOLDER_PATH, "database");
-const DSU_FABRIC_ENCLAVE_MIGRATED_PATH = `${DSU_FABRIC_ENCLAVE_FOLDER_PATH}.migrated`;
+const apihubModule = require("apihub");
 
+// Utility function for checking if migration is already done
+const isMigrationDone = (migratedPath) => {
+    try {
+        fs.accessSync(migratedPath);
+        console.log(`${path.basename(migratedPath)} already migrated`);
+        return true;
+    } catch (e) {
+        return false;  // Continue migration
+    }
+};
+
+// Utility function for renaming folders and updating file contents
+const renameFolderAndUpdateFile = (sourcePath, renamedPath, folderName, renamedFolderName) => {
+    // check if the source folder is empty
+    let files;
+    try {
+        files = fs.readdirSync(sourcePath);
+    } catch (e) {
+        console.error(`Failed to read folder ${sourcePath}`, e);
+        throw e;
+    }
+    // If the folder is empty, do not proceed with renaming
+    if (files.length === 0) {
+        return;
+    }
+
+    try {
+        fs.renameSync(sourcePath, renamedPath);
+    } catch (e) {
+        console.error(`Failed to rename folder ${renamedPath}`, e);
+        throw e;
+    }
+
+    const filePath = path.join(renamedPath, 'database');
+    const fileContent = fs.readFileSync(filePath);
+    const newContent = fileContent.toString().replace(folderName, renamedFolderName);
+    fs.writeFileSync(filePath, newContent);
+};
+
+// Utility function for migrating a table from one database to another
 const migrateTable = async (structuredLokiEnclaveFacadeInstance, tableName, partitionedLokiEnclaveFacadeInstance) => {
     let records;
     try {
         records = await $$.promisify(structuredLokiEnclaveFacadeInstance.getAllRecords)(undefined, tableName);
     } catch (e) {
-        console.error("Failed to get records from table", tableName, e);
+        console.error(`Failed to get records from table ${tableName}`, e);
         throw e;
-    }
-
-    if (records && records.length > 0) {
-        console.log(records.map(record => record.$loki));
     }
 
     for (let record of records) {
@@ -32,69 +58,77 @@ const migrateTable = async (structuredLokiEnclaveFacadeInstance, tableName, part
             delete record.$loki;
             await $$.promisify(partitionedLokiEnclaveFacadeInstance.insertRecord)($$.SYSTEM_IDENTIFIER, tableName, record.pk, record);
         } catch (e) {
-            console.error("Failed to insert record", record, "in table", tableName, e);
+            console.error(`Failed to insert record in table ${tableName}`, e);
             throw e;
         }
     }
-}
+};
+
+// Utility function for migrating all tables
 const migrateAllTables = async (structuredLokiEnclaveFacadeInstance, partitionedLokiEnclaveFacadeInstance) => {
     const tables = await $$.promisify(structuredLokiEnclaveFacadeInstance.getCollections)($$.SYSTEM_IDENTIFIER);
     for (let table of tables) {
-        try {
-            await migrateTable(structuredLokiEnclaveFacadeInstance, table, partitionedLokiEnclaveFacadeInstance);
-        } catch (e) {
-            console.error("Failed to migrate table", table, e);
-            throw e;
-        }
+        await migrateTable(structuredLokiEnclaveFacadeInstance, table, partitionedLokiEnclaveFacadeInstance);
     }
-}
+};
 
-const migrateStructuredAdapterToPartitionedAdapter = async () => {
-    try {
-        fs.accessSync(DSU_FABRIC_ENCLAVE_MIGRATED_PATH);
-        console.log("DSU Fabric Enclave already migrated");
-        return;
-    } catch (e) {
-        // continue with migration
-    }
-    //check if the enclave folder exists
-    try {
-        fs.accessSync(DSU_FABRIC_ENCLAVE_FOLDER_PATH);
-    } catch (e) {
-        // nothing to migrate
-        fs.writeFileSync(DSU_FABRIC_ENCLAVE_MIGRATED_PATH, "");
+// Utility function to handle the migration of any database
+const migrateDatabase = async (folderPath, renamedFolderPath, migratedPath, folderName, renamedFolderName) => {
+    if (isMigrationDone(migratedPath)) {
         return;
     }
-    // rename the folder to avoid conflicts
+
     try {
-        fs.renameSync(DSU_FABRIC_ENCLAVE_FOLDER_PATH, DSU_FABRIC_ENCLAVE_RENAMED_FOLDER_PATH);
+        fs.accessSync(folderPath);
     } catch (e) {
-        console.error("Failed to rename folder", DSU_FABRIC_ENCLAVE_RENAMED_FOLDER_PATH, e);
+        // No folder to migrate, create a migrated file
+        fs.writeFileSync(migratedPath, "");
         return;
     }
-    // replace the filename in database file
-    const fileContent = fs.readFileSync(DSU_FABRIC_ENCLAVE_RENAMED_PATH);
-    const newContent = fileContent.toString().replace(DSU_FABRIC_ENCLAVE_FOLDER_NAME, DSU_FABRIC_ENCLAVE_RENAMED_FOLDER_NAME);
-    fs.writeFileSync(DSU_FABRIC_ENCLAVE_RENAMED_PATH, newContent);
 
-    const structuredLokiEnclaveFacade = LokiEnclaveFacade.createLokiEnclaveFacadeInstance(DSU_FABRIC_ENCLAVE_RENAMED_PATH, undefined, adapters.STRUCTURED);
+    renameFolderAndUpdateFile(folderPath, renamedFolderPath, folderName, renamedFolderName);
 
-    try {
-        fs.mkdirSync(DSU_FABRIC_ENCLAVE_FOLDER_PATH, {recursive: true});
-    } catch (e) {
-        // if folder exists do nothing else throw error
-        if (e.code !== "EEXIST") {
-            throw e;
-        }
-    }
+    const structuredLokiEnclaveFacade = LokiEnclaveFacade.createLokiEnclaveFacadeInstance(path.join(renamedFolderPath, 'database'), undefined, LokiEnclaveFacade.Adapters.STRUCTURED);
+    const partitionedLokiEnclaveFacade = LokiEnclaveFacade.createLokiEnclaveFacadeInstance(path.join(folderPath, 'database'), undefined, LokiEnclaveFacade.Adapters.FS);
 
-    const partitionedLokiEnclaveFacade = LokiEnclaveFacade.createLokiEnclaveFacadeInstance(DSU_FABRIC_ENCLAVE_PATH, undefined, adapters.PARTITIONED);
     await migrateAllTables(structuredLokiEnclaveFacade, partitionedLokiEnclaveFacade);
     await $$.promisify(partitionedLokiEnclaveFacade.saveDatabase)($$.SYSTEM_IDENTIFIER);
-    // fs.rmSync(DSU_FABRIC_ENCLAVE_RENAMED_FOLDER_PATH, {recursive: true});
-    fs.writeFileSync(DSU_FABRIC_ENCLAVE_MIGRATED_PATH, "");
-    console.info(0x333, "DSU Fabric Enclave migration completed");
-}
 
-module.exports = migrateStructuredAdapterToPartitionedAdapter;
+    fs.writeFileSync(migratedPath, "");
+    console.info(`${folderName} migration completed`);
+};
+
+const apihubRootFolder = apihubModule.getServerConfig().storage;
+const lightDBPath = path.join(apihubRootFolder, "external-volume/lightDB");
+// Specific migration processes for DSU_Fabric and FixedUrls.db
+const migrateDSUFabric = async () => {
+    const dsuFolderName = `DB_${process.env.EPI_DOMAIN}_${process.env.EPI_SUBDOMAIN}`;
+    const dsuRenamedFolderName = 'dsu-fabric-enclave-renamed';
+
+    const dsuFolderPath = path.join(lightDBPath, dsuFolderName);
+    const dsuRenamedFolderPath = path.join(lightDBPath, dsuRenamedFolderName);
+    const dsuMigratedPath = `${dsuFolderPath}.migrated`;
+
+    await migrateDatabase(dsuFolderPath, dsuRenamedFolderPath, dsuMigratedPath, dsuFolderName, dsuRenamedFolderName);
+};
+
+const migrateFixedUrlsDB = async () => {
+    const fixedUrlsFolderName = 'FixedUrls.db';
+    const fixedUrlsRenamedFolderName = 'FixedUrls.db-renamed';
+
+    const fixedUrlsFolderPath = path.join(lightDBPath, fixedUrlsFolderName);
+    const fixedUrlsRenamedFolderPath = path.join(lightDBPath, fixedUrlsRenamedFolderName);
+    const fixedUrlsMigratedPath = `${fixedUrlsFolderPath}.migrated`;
+
+    await migrateDatabase(fixedUrlsFolderPath, fixedUrlsRenamedFolderPath, fixedUrlsMigratedPath, fixedUrlsFolderName, fixedUrlsRenamedFolderName);
+};
+
+// Combined Migration Process
+const migrateAll = async () => {
+    await migrateDSUFabric();
+    await migrateFixedUrlsDB();
+    console.info(0x333, "Both DSU Fabric and FixedUrls.db migrations completed.");
+};
+
+module.exports = migrateAll;
 
