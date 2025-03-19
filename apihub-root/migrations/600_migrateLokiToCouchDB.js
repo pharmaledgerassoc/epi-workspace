46552b78f6d729494327900496455871d6101e0b
vnogueira <vitor.nogueira@pdmfc.com>
2025-03-19 14:45:54 +0000

    TRUST-407 - updated

--- apihub-root/migrations/600_migrateLokiToCouchDB.js
+++ apihub-root/migrations/600_migrateLokiToCouchDB.js
@@ -5,7 +5,11 @@ require("opendsu");
 const path = require('path');
 const fs = require('fs');
 
-const apihubRootFolder = apihubModule.getServerConfig().storage;
+const DBService = EnclaveFacade.DBService;
+
+const config = apihubModule.getServerConfig();
+const apihubRootFolder = config.storage;
+
 
 const DATABASE = "database";
 const STORAGE_LOCATION = path.join(apihubRootFolder, "/external-volume/lightDB");
@@ -13,30 +17,36 @@ const COUCH_DB_MIGRATED_FILE = STORAGE_LOCATION + "/couchdb.migrated";
 
 
 const ANCHORS_TABLE_NAME = "anchors_table";
-const ANCHORS_TABLE_INDEXES = ["scheduled"];
+const ANCHORS_TABLE_INDEXES = "scheduled";
 
 const FIXED_URL_TABLE_NAMES = ["history", "tasks"];
-const FIXED_URL_TABLE_INDEXES = ["url"];
+const FIXED_URL_TABLE_INDEXES = "url"
+
+let dbService;
 
 const mapRecordToCouchDB = (record) => {
     delete record.meta;
     delete record.$loki;
-    let timestamp = record.__timestamp
+    record["timestamp"] = record.__timestamp;
+    record["_rev"] = record.__version || 0;
     delete record.__timestamp;
-    record["timestamp"] = timestamp;
+    delete record.__version;
+
+    if(!record.timestamp)
+        delete record.timestamp;
 
     return record
 }
 
-const migrateTable = async (couchDB, tableName, records) => {
+const migrateTable = async (dbPath, tableName, records) => {
 
-    for (let record in records) {
+    for (let record of records) {
         try {
             const processedRecord = mapRecordToCouchDB(record)
 
-            await $$.promisify(couchDB.insertRecord)($$.SYSTEM_IDENTIFIER, tableName, processedRecord.pk, processedRecord);
+            await insertRecord(dbPath, tableName, processedRecord.pk, processedRecord);
         } catch (e) {
-            console.error("Failed to migrate table", table, e);
+            console.error("Failed to migrate table", tableName, e);
             throw e;
         }
      
@@ -45,33 +55,68 @@ const migrateTable = async (couchDB, tableName, records) => {
 
 }
 
+const insertRecord = async (dbPath, tableName, pk, record) => {
+    try {
+        let dbName = await getDbName(dbPath, tableName);
+        dbName = dbService.changeDBNameToLowerCaseAndValidate(dbName);
+    
+        const exists = await dbService.dbExists(dbName);
+    
+        if (!exists)
+            throw new Error(`Database Doesn't exist: ${dbName}! Failed to migrate!`);
+
+        await dbService.insertDocument(dbName, pk, record)
+    } catch (e) {
+        throw e
+    }
+}
+
+const createCollection = async (dbPath, tableName, indexes) => {
+    try {
+        let dbName = await getDbName(dbPath, tableName);
+        dbName = dbService.changeDBNameToLowerCaseAndValidate(dbName);
+    
+        const exists = await dbService.dbExists(dbName);
+    
+        if (exists)
+            throw new Error(`Database already exists: ${dbName}! If DB Exists It means the migration should already happened!`);
+
+        await dbService.createDatabase(dbName, indexes);
+    } catch (e) {
+        throw e
+    }
+}
+
+const getDbName = async (dbPath, tableName) => {
+    const prefix = dbPath.includes("/")
+        ? dbPath.split("/").slice(dbPath.split("/").length -2, dbPath.split("/").length -1)[0]
+        : dbPath;
+
+    return ["db", prefix, tableName].filter(e => !!e).join("_");
+}
+
 
 const migrate = async (dbPath) => {
     const db = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
     const tables = db.collections
 
-    const couchDB = EnclaveFacade.createCouchDBEnclaveFacadeInstance(dbLocation);
-
     for (let table of tables){
-        let indexes = ["pk", "timestamp"]
+        let indexes = ["pk", "timestamp"];
 
         if(ANCHORS_TABLE_NAME === table.name)
-            indexes.push(ANCHORS_TABLE_INDEXES)
+            indexes.push(ANCHORS_TABLE_INDEXES);
 
         if(FIXED_URL_TABLE_NAMES.includes(table.name))
-            indexes.push(FIXED_URL_TABLE_INDEXES)
+            indexes.push(FIXED_URL_TABLE_INDEXES);
 
-        await $$.promisify(couchDB.storageDB.createCollection)(undefined, table.name, indexes);
-
-        await migrateTable(couchDB, table.name, table.data);
-        
-
+        await createCollection(dbPath, table.name, indexes);
+        await migrateTable(dbPath, table.name, table.data);
     }
 
 }
 
 const migrateLokiToCouchDB = async () => {
-    return 
+    return
     try {
         fs.mkdirSync(path.dirname(STORAGE_LOCATION), {recursive: true});
     } catch (e) {
@@ -88,6 +133,13 @@ const migrateLokiToCouchDB = async () => {
     } catch (e) {
         // continue with migration
     }
+
+
+    dbService = new DBService( {
+        uri: config.db.uri,
+        username: config.db.user,
+        secret: config.db.secret,
+    });
 
     let folders;
 
