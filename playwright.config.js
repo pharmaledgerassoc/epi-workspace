9fbf708de2f8ccd6861528decdf1dc005434eac3
Demerson Máximo de Carvalho <dcarvalho@pdmfc.com>
2025-03-27 14:52:12 -0300

    TRUST-207 - add playwright test

---
+++ playwright.config.js
@@ -0,0 +1,46 @@
+const { devices, defineConfig } = require('@playwright/test');
+
+module.exports =  defineConfig({
+    testDir: './tests/playwright', // Caminho para a pasta onde os testes estão localizados
+    timeout: 30000,     // Tempo limite para cada teste (em milissegundos)
+    retries: 2,         // Número de tentativas em caso de falha
+    use: {
+      permissions: ['geolocation'],
+      headless: true,   // Executar os testes no modo headless
+      channel: 'chrome' // Configura o navegador para usar o Google Chrome
+    },
+    projects: [
+        {
+            name: 'Chrome',
+            use: {
+                ...devices['Desktop Chrome'],
+                launchOptions: {
+                    args: ['--disable-web-security',
+                        '--use-fake-ui-for-media-stream',
+                        '--use-fake-device-for-media-stream',
+                    ],
+                }
+            },
+        },
+
+        // {
+        //     name: 'Mobile Chrome',
+        //     use: {
+        //         ...devices['Pixel 5'],
+        //         launchOptions: {
+        //             args: [],
+        //         }
+        //     },
+        // },
+
+        // {
+        //     name: 'Mobile Safari',
+        //     use: {
+        //         ...devices['iPhone 13'],
+        //         launchOptions: {
+        //             args: [],
+        //         }
+        //     },
+        // },
+    ],
+})
