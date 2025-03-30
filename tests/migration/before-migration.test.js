b33e4028c0baff9f7e4888488e8bd6da9bb24be2
vnogueira91 <vitor.nogueira@pdmfc.com>
2025-03-30 16:53:24 +0100

    TRUST-407 - added leaflets to before migration

--- tests/migration/before-migration.test.js
+++ tests/migration/before-migration.test.js
@@ -145,7 +145,29 @@ describe(`TRUST-125 Before Migration Test`, () => {
     const productResponse = await client.getProduct(product.productCode);
     expect(productResponse.data).toEqual(expect.objectContaining(product));
 
-    //TODO: Check audit validate Add leaflet  and cache it
+    //TODO: Check audit for product and leaflet validate and cache it
+
+    const LANG = "en";
+    const XML_FILE_WITH_IMG = (fs.readFileSync(path.join(__dirname, "..", "resources", "xml_content_with_img.txt"), {encoding: 'utf-8'})).trim();
+    const IMG_FILE_NAME = "figure_010_1295_1485_3620_1050.png";
+    const IMG_FILE_CONTENT = (fs.readFileSync(path.join(__dirname, "..", "resources", "figure_010_1295_1485_3620_1050.png.txt"), {encoding: 'utf-8'})).trim();
+    
+
+    const leaflet = new Leaflet({
+      productCode: product.productCode,
+      language: LANG,
+      xmlFileContent: XML_FILE_WITH_IMG,
+      otherFilesContent: [{
+          filename: IMG_FILE_NAME,
+          fileContent: IMG_FILE_CONTENT
+      }]
+    });
+
+    const resleaf = await client.addLeaflet(leaflet.productCode, undefined, leaflet.language, "leaflet", undefined, leaflet);
+    expect(resleaf.status).toBe(200);
+    
+
+
   });
 
   it("SUCCESS 200 - Should create a basic batch properly", async () => {
@@ -207,6 +229,26 @@ describe(`TRUST-125 Before Migration Test`, () => {
     );
     expect(batchResponse.data).toEqual(expect.objectContaining(batch));
 
-    //TODO: validate audit add leaflet
+    //TODO: validate audit batch and leaflet
+
+    const LANG = "en";
+    const XML_FILE_WITH_IMG = (fs.readFileSync(path.join(__dirname, "..", "resources", "xml_content_with_img.txt"), {encoding: 'utf-8'})).trim();
+    const IMG_FILE_NAME = "figure_010_1295_1485_3620_1050.png";
+    const IMG_FILE_CONTENT = (fs.readFileSync(path.join(__dirname, "..", "resources", "figure_010_1295_1485_3620_1050.png.txt"), {encoding: 'utf-8'})).trim();
+
+    const leaflet = new Leaflet({
+      productCode: batch.productCode,
+      batchNumber: batch.batchNumber,
+      language: LANG,
+      xmlFileContent: XML_FILE_WITH_IMG,
+      otherFilesContent: [{
+          filename: IMG_FILE_NAME,
+          fileContent: IMG_FILE_CONTENT
+      }]
+    });
+    
+    const resleaflet = await client.addLeaflet(leaflet.productCode, leaflet.batchNumber, leaflet.language, "leaflet", undefined, leaflet);
+    expect(resleaflet.status).toBe(200);
+    
   });
 });
